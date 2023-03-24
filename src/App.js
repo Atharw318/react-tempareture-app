3d1fcaede43add855a8cd205514e8e18f58e3ea6
atharw <atharw010@gmail.com>
2023-03-24 15:46:59 +0530

    changes temp color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tempvalue, setTempvalue] = useState(20);
+  const [tempcolor, setTempcolor] = useState('hot');
+  const increstemp=() =>{
+
+    const newtemp = tempvalue+1
+
+    if (newtemp>=15){
+      setTempcolor('hot')
+    }
+
+    if (newtemp<=30) {
+      setTempvalue(newtemp)
+    }
+  
+  }
+  const decrestemp =() =>{
+    const newtemp = tempvalue-1
+
+    if (newtemp<=15) {
+      setTempcolor('cold')
+    }
+    
+    if (newtemp>=0) {
+      setTempvalue(newtemp);
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='tempbox'>
+      <div className= {`displaytemp ${tempcolor}`}>{tempvalue}</div>
+      <div className='button'>
+        <button onClick={() => increstemp()} className='button1'>+</button>
+        
+        <button onClick={() => decrestemp()} className='button2'>-</button>
+      </div>
+     </div>
     </div>
   );
 }
